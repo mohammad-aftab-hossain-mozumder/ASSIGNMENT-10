8ce55e456998edac77e1d06edcc69eb8fca65570
MD-Aftab-Hossain <md.aftab.hn@gmail.com>
2025-11-13 21:17:47 +0600

    last 2

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -32,7 +32,7 @@ export default function Details() {
 
   const [my, setmy] = useState([])
   useEffect(() => {
-    // if (!data?.artistemail) { return }
+    // if (!data?.artistemail) { return 
     setloading(true)
     const datass = fetch(`https://assignment-10-server-eta-eight.vercel.app/posts/single?email=${data?.artistemail}`).then(res => res.json())
       .then(ans => {
