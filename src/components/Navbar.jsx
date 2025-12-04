b820fcee463538a3af1021478d2a8f75e67c5514
MD-Aftab-Hossain <md.aftab.hn@gmail.com>
2025-12-04 11:47:20 +0600

    last

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                 <li><div className=""><input onChange={(e) => handletheme(e.target.checked)} defaultChecked={localStorage.getItem("theme") === "dark"} type="checkbox" value="synthwave" className="toggle theme-controller" /></div></li>
             </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar sticky top-0 z-50 bg-base-100 shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
